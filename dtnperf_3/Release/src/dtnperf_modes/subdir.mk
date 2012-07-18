16fa6be9683161db2ddc276502195b295055a4d9
michirod <michirod@gmail.com>
2012-07-18 10:17:50 +0200

    makefile

---
+++ dtnperf_3/Release/src/dtnperf_modes/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/dtnperf_modes/dtnperf_client.c \
+../src/dtnperf_modes/dtnperf_monitor.c \
+../src/dtnperf_modes/dtnperf_server.c 
+
+OBJS += \
+./src/dtnperf_modes/dtnperf_client.o \
+./src/dtnperf_modes/dtnperf_monitor.o \
+./src/dtnperf_modes/dtnperf_server.o 
+
+C_DEPS += \
+./src/dtnperf_modes/dtnperf_client.d \
+./src/dtnperf_modes/dtnperf_monitor.d \
+./src/dtnperf_modes/dtnperf_server.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/dtnperf_modes/%.o: ../src/dtnperf_modes/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -I/home/michele/Dropbox/workspace/bp_abstraction_layer/src -I/home/michele/Dropbox/git/bp-abstraction-api/bp_abstraction_layer/src -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
