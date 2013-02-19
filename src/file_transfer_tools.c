cec4a3fa09409020961d6e196727238c4afa1560
annettaws <anna.damico.ws@gmail.com>
2013-02-19 11:40:46 +0000

    ok

--- src/file_transfer_tools.c
+++ src/file_transfer_tools.c
@@ -193,10 +193,8 @@ int assemble_file(file_transfer_info_t * info, FILE * pl_stream,
 
 	printf("\n\tRECV: %lu - TOT: %lu\n",info->bytes_recvd,info->file_dim);
 	// if transfer completed return 1
-	if (info->bytes_recvd >= info->file_dim){
-		printf("\n\t\tCOMPLETATO\n");
+	if (info->bytes_recvd >= info->file_dim)
 		return 1;
-	}
 	return 0;
 
 }
@@ -338,6 +336,7 @@ al_bp_error_t prepare_file_transfer_payload(dtnperf_options_t *opt, FILE * f, in
 	fwrite(&offset, sizeof(offset), 1, f);
 	// read fragment from file
 	bytes_read = read(fd, fragment, fragment_len);
+	printf("\nBytes Read: %lu - Frag: %lu\n",bytes_read,fragment_len);
 	if (bytes_read < fragment_len) // reached EOF
 		*eof = TRUE;
 	else
