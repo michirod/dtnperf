bb9e52af499ab9faec252701bfe5cf92e0261d65
annettaws <anna.damico.ws@gmail.com>
2013-02-23 14:27:49 +0000

    SABATO

--- Debug/src/utils.d
+++
@@ -1,8 +0,0 @@
-src/utils.d: ../src/utils.c ../src/utils.h ../src/includes.h \
- /home/annetta/DTN/bp_abstraction_layer/bp_abstraction_layer/src/types.h
-
-../src/utils.h:
-
-../src/includes.h:
-
-/home/annetta/DTN/bp_abstraction_layer/bp_abstraction_layer/src/types.h:
