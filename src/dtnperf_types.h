0aa51eb730bdf4cba244516a3aac7bf51cc1ec0e
annettaws <anna.damico.ws@gmail.com>
2013-03-14 18:02:14 +0000

    No Forced Option

--- src/dtnperf_types.h
+++ src/dtnperf_types.h
@@ -54,8 +54,6 @@ typedef struct dtnperf_options
 	boolean_t use_ip;		// set different values of ip address and port [FALSE]
 	char * ip_addr;			// daemon ip address [127.0.0.1]
 	short ip_port;			// daemon port [5010]
-	boolean_t force_eid;    // if true, the value of eid_forced indicated the eid to force. [FALSE]
-	char eid_forced;        // eid to force (U = uri, C = cbhe) [U]
 	boolean_t daemon;		// run as daemon (server and monitor) [FALSE]
 	char * server_output_file;	// stdout and stderr redirect here if daemon is TRUE [SERVER_OUTPUT_FILE]
 	char * monitor_output_file;	// stdout and stderr redirect here if daemon is TRUE [MONITOR_OUTPUT_FILE]
