5ef493746c1f0b4746387e3882fd4c9cd401a4dc
annettaws <anna.damico.ws@gmail.com>
2013-03-14 16:10:11 +0000

    Update 14 16.10

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -29,7 +29,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	gcc -I"/home/annetta/test_VM/abstraction_layer_bundle_protocol" -I/home/annetta/DTN/bp_abstraction_layer/bp_abstraction_layer/src -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/DTN/bp_abstraction_layer" -I/home/annetta/DTN/bp_abstraction_layer/bp_abstraction_layer/src -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
