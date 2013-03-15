7c4ed036f1b1d815dd154700ed16699f5ce01158
annettaws <anna.damico.ws@gmail.com>
2013-03-15 12:16:52 +0000

    Forced Option Only Server

--- src/dtnperf_types.h
+++ src/dtnperf_types.h
@@ -54,7 +54,7 @@ typedef struct dtnperf_options
 	boolean_t use_ip;		// set different values of ip address and port [FALSE]
 	char * ip_addr;			// daemon ip address [127.0.0.1]
 	short ip_port;			// daemon port [5010]
-	char eid_format_forced;		// is the format of the eid (U = URI, C = CBHE, N = None)
+	char eid_format_forced;	// is the format of the eid (U = URI, C = CBHE, N = None) [N]
 	boolean_t daemon;		// run as daemon (server and monitor) [FALSE]
 	char * server_output_file;	// stdout and stderr redirect here if daemon is TRUE [SERVER_OUTPUT_FILE]
 	char * monitor_output_file;	// stdout and stderr redirect here if daemon is TRUE [MONITOR_OUTPUT_FILE]
