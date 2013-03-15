3ad0dc69f98865171193910465dbe3dc04eebb1d
annettaws <anna.damico.ws@gmail.com>
2013-03-15 11:45:10 +0000

    Test option

--- src/dtnperf_modes/dtnperf_client.c
+++ src/dtnperf_modes/dtnperf_client.c
@@ -115,12 +115,12 @@ void run_dtnperf_client(dtnperf_global_options_t * perf_g_opt)
 	log_open = FALSE;
 	bp_handle_open = FALSE;
 	source_file_created = FALSE;
-//	stream = NULL;
 	tot_bundles = 0;
 	process_interrupted = FALSE;
 	perf_opt->log_filename = correct_dirname(perf_opt->log_filename);
-//	source_file = (char*) malloc(strlen(SOURCE_FILE) + 7);
-//	sprintf(source_file, "%s_%d", SOURCE_FILE, getpid());
+
+	printf("TEST: %d\n", perf_opt->test);
+
 
 	// Create a new log file
 	if (create_log)
@@ -788,7 +788,7 @@ void * send_bundles(void * opt)
 	if (perf_opt->op_mode == 'T') 	// TIME MODE
 	{								// init variables for loop and setting condition
 		now.tv_sec = start.tv_sec;
-		condition = now.tv_sec < end.tv_sec;
+		condition = now.tv_sec <= end.tv_sec;
 	}
 	else							// DATA and FILE MODE
 	{								// setting condition for loop
@@ -883,7 +883,7 @@ void * send_bundles(void * opt)
 		if (perf_opt->op_mode == 'T')	// TIME MODE
 		{								// update time and condition
 			gettimeofday(&now, NULL);
-			condition = now.tv_sec < end.tv_sec;
+			condition = now.tv_sec <= end.tv_sec;
 			printf("\n\tNOW sec: %lu - CONDITION %d\n",now.tv_sec,condition);
 		}
 		else							// DATA MODE
