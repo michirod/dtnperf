61310263001fff54136c0bde372924e5c0555044
annettaws <anna.damico.ws@gmail.com>
2013-03-15 10:22:05 +0000

    Bug Time

--- src/dtnperf_modes/dtnperf_client.c
+++ src/dtnperf_modes/dtnperf_client.c
@@ -884,6 +884,7 @@ void * send_bundles(void * opt)
 		{								// update time and condition
 			gettimeofday(&now, NULL);
 			condition = now.tv_sec <= end.tv_sec;
+			printf("\n\tNOW sec: %lu - CONDITION %d\n",now.tv_sec,condition);
 		}
 		else							// DATA MODE
 		{								// update condition
