2b0f9325d7ab53434e822f78b746d62f67fbe2a0
annettaws <anna.damico.ws@gmail.com>
2013-04-09 17:40:45 +0100

    Exit bug

--- src/dtnperf_modes/dtnperf_client.c
+++ src/dtnperf_modes/dtnperf_client.c
@@ -539,8 +539,6 @@ void run_dtnperf_client(dtnperf_global_options_t * perf_g_opt)
 	if(perf_opt->create_log)
 		print_final_report(log_file);
 
-	sleep(2);
-
 	// fill the stop bundle
 	prepare_stop_bundle(&bundle_stop, mon_eid, conn_opt->expiration, conn_opt->priority, sent_bundles);
 	al_bp_bundle_set_source(&bundle_stop, local_eid);
