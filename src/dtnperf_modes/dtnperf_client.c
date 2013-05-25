fc2b1b809c7158a6ad8567218a608b9fe5a04acc
annettaws <anna.damico.ws@gmail.com>
2013-05-25 11:02:20 +0100

    Test Ora

--- src/dtnperf_modes/dtnperf_client.c
+++ src/dtnperf_modes/dtnperf_client.c
@@ -651,15 +651,11 @@ void run_dtnperf_client(dtnperf_global_options_t * perf_g_opt)
 		{
 			tmp_payload.filename.filename_len = strlen(file_bundle_names[i]);
 			tmp_payload.filename.filename_val = file_bundle_names[i];
-			printf("FREE %s\n", file_bundle_names[i]);
 			al_bp_free_payload(&tmp_payload);
 		}
-		//free(file_bundle_names);
-		printf("\nOK-OK\n");
 	}
 	//structure bundle is always free in every op mode
 	al_bp_bundle_free(&bundle);
-	printf("\nOK-OK-OK\n");
 	al_bp_bundle_free(&bundle_stop);
 
 	if (perf_opt->create_log)
