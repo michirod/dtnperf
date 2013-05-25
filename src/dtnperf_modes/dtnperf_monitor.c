23475af6723597a3eb48c0b17f8f68da383beb14
annettaws <anna.damico.ws@gmail.com>
2013-05-25 09:49:21 +0100

    Bundles_file_array

--- src/dtnperf_modes/dtnperf_monitor.c
+++ src/dtnperf_modes/dtnperf_monitor.c
@@ -649,7 +649,7 @@ void print_monitor_usage(char * progname)
 			" -a, --daemon                  Start the monitor as a daemon. Output is redirected to %s.\n"
 			" -o, --output <file>           Change the default output file (Only with -a option).\n"
 			" -s, --stop                    Stop a demonized instance of monitor.\n"
-			" -e, --expiration-session <s>  Max idle time of log files (s). Default: 60.\n"
+			" -e, --session-expiration <s>  Max idle time of log files (s). Default: 60.\n"
 			"     --ip-addr <addr>          Ip address of the bp daemon api. Default: 127.0.0.1 (DTN2 only)\n"
 			"     --ip-port <port>          Ip port of the bp daemon api. Default: 5010 (DTN2 only)\n"
 			"     --force-eid <[DTN|IPN]    Force the registration EID independently of BP implementation. (ION only)\n"
@@ -682,7 +682,7 @@ void parse_monitor_options(int argc, char ** argv, dtnperf_global_options_t * pe
 					{"ip-addr", required_argument, 0, 37},
 					{"ip-port", required_argument, 0, 38},
 					{"force-eid", required_argument, 0, 51},
-					{"expiration-session", required_argument, 0,'e'},
+					{"session-expiration", required_argument, 0,'e'},
 					{"daemon", no_argument, 0, 'a'},
 					{"output", required_argument, 0, 'o'},
 					{"stop", no_argument, 0, 's'},
