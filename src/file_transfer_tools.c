16f30a1f0bfb688b299490c5bc3a217d8be10821
annettaws <anna.damico.ws@gmail.com>
2013-05-29 20:05:26 +0100

    memset 0

--- src/file_transfer_tools.c
+++ src/file_transfer_tools.c
@@ -353,7 +353,7 @@ al_bp_error_t prepare_file_transfer_payload(dtnperf_options_t *opt, FILE * f, in
 	// get size of fragment and allocate fragment
 	fragment_len = get_file_fragment_size(opt->bundle_payload, filename_len, monitor_eid_len);
 	fragment = (char *) malloc(fragment_len);
-	memset(fragment,'\0',fragment_len);
+	memset(fragment,0,fragment_len);
 	// get offset of fragment
 	offset = lseek(fd, 0, SEEK_CUR);
 	// write offset in the bundle
