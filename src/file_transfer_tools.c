24492e98c993a1b2851f6528d86d6960d098b3db
annettaws <anna.damico.ws@gmail.com>
2013-05-30 16:29:14 +0100

    ok

--- src/file_transfer_tools.c
+++ src/file_transfer_tools.c
@@ -165,7 +165,7 @@ int assemble_file(file_transfer_info_t * info, FILE * pl_stream,
 	transfer_len = get_file_fragment_size(pl_size, info->filename_len, monitor_eid_len);
 	// read file fragment offset
 	fread(&offset, sizeof(offset), 1, pl_stream);
-	printf("\nOFFSET: %lu\nDIM FRAG: %lu\n", offset, transfer_len);
+	printf("\nOFFSET: %ld\nDIM FRAG: %ld\n", offset, transfer_len);
 	// read remaining file fragment
 	transfer = (char*) malloc(transfer_len);
 	memset(transfer, 0, transfer_len);
@@ -357,7 +357,7 @@ al_bp_error_t prepare_file_transfer_payload(dtnperf_options_t *opt, FILE * f, in
 	fwrite(&offset, sizeof(offset), 1, f);
 	// read fragment from file
 	bytes_read = read(fd, fragment, fragment_len);
-	printf("\nOFFSET: %lu\nDIM FRAG: %lu\n", offset, fragment_len);
+	printf("\nOFFSET: %ld\nDIM FRAG: %ld\n", offset, fragment_len);
 	if (bytes_read < fragment_len)// reached EOF
 	{
 		*eof = TRUE;
