623dbd9404829be8687c4bea80a817fb4a5fd9cb
annettaws <anna.damico.ws@gmail.com>
2013-06-03 00:27:58 +0100

    Sleep Only ION

--- src/dtnperf_modes/dtnperf_client.c
+++ src/dtnperf_modes/dtnperf_client.c
@@ -1122,9 +1122,8 @@ void * congestion_control(void * opt)
 			if ((debug) && (debug_level > 0))
 				printf("\t[debug cong ctrl] waiting for the reply...\n");
 
-			printf("BEFORE SLEEP\n");
-			pthread_sleep(0.5);
-			printf("AFTER SLEEP\n");
+			if(perf_opt->bp_implementation == BP_ION)
+				pthread_sleep(0.5);
 			if ((error = al_bp_bundle_receive(handle, ack, BP_PAYLOAD_MEM,
 					count_info(send_info, perf_opt->window) == 0 ? perf_opt->wait_before_exit : -1))
 					!= BP_SUCCESS)
